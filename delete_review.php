81ab5cda4ef60a3ceb4844bc9f955f416215dcc5
bellamaharanni <135285217+bellamaharanni@users.noreply.github.com>
2024-05-06 09:23:42 +0700

    membuat tampilan review

---
+++ delete_review.php
@@ -0,0 +1,17 @@
+<?php
+$koneksi = mysqli_connect("localhost", "root", "", "ecoscore");
+
+if (mysqli_connect_errno()) {
+    die("Koneksi database gagal: " . mysqli_connect_error());
+}
+
+if (isset($_GET['id'])) {
+    $id = intval($_GET['id']);
+    $query = "DELETE FROM reviews WHERE id = ?";
+    $stmt = mysqli_prepare($koneksi, $query);
+    mysqli_stmt_bind_param($stmt, 'i', $id);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+}
+
+mysqli_close($koneksi);
